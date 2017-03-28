75a6f1aa49df7929695246fd7de0ecd6b5116a2f
Kate <xkate93@live.com>
2017-03-28 23:24:34 +0300

    complete implementation of backend - refinement needed

--- src/BranchInfo.java
+++ src/BranchInfo.java
@@ -1,16 +1,20 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class BranchInfo {
-	String BranchName;
-	String CreationDate;
-	String LastModified;
-	ArrayList<CommitInfo> CommitList;
+	String branchName;
+	String creationDate;
+	String lastModified;
+	Double commitPercentage;
+	ArrayList<CommitInfo> commitList;
+	HashMap<String,Double> percentPerCommiter;
 	
 	public BranchInfo(){
-		BranchName = null;
-		CreationDate = null;
-		LastModified = null;
-		CommitList = new ArrayList<CommitInfo>();
+		branchName = null;
+		creationDate = null;
+		lastModified = null;
+		commitList = new ArrayList<CommitInfo>();
+		percentPerCommiter = new HashMap<String,Double>();
 	}
 }
 
@@ -19,11 +23,25 @@ class CommitInfo {
 	String message;
 	String date;
 	String author;
+	String tag;
 	
 	public CommitInfo(){
 		id = null;
 		message = null;
 		date = null;
 		author = null;
+		tag = null;
+	}
+}
+
+class Committer{
+	Integer numOfCommits;
+	Double commitPercentage;
+	Double commitsPerDay;
+	Double commitsPerMonth;
+	Double commitsPerYear;
+	
+	public Committer(){
+		numOfCommits = 0;
 	}
 }
--- src/analysis.java
+++ src/analysis.java
@@ -1,3 +1,4 @@
+import java.awt.List;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -9,10 +10,19 @@ import java.util.Set;
 
 public class analysis {
 	
+	static HashMap<String,Committer> committers;
+	static ArrayList<String> branches;
+	static Integer CommitCount;
+	static ArrayList<BranchInfo> branchInfoArray;
+	static Integer numOfInsertions;
+	static Integer numOfDeletions;
+	
 	public static void main(String[] args) throws IOException{
 		
-		HashMap<String,Integer> commiters = new HashMap<String,Integer>(); 
-		ArrayList<String> branches = new ArrayList<String>();
+		numOfInsertions = 0;
+		numOfDeletions = 0;
+		committers = new HashMap<String,Committer>(); 
+		branches = new ArrayList<String>();
 		String directory = "C:\\Users\\Aruil\\Documents\\temp_git_repo\\jgit";
 		Process proc = null;
 		ProcessBuilder pb;
@@ -64,7 +74,10 @@ public class analysis {
 		 //   System.out.println(s);
 		    num_of_branches++;
 		    String[] cleanstr = s.split(" ");
-		    branches.add(cleanstr[1]);
+		    if(cleanstr.length > 2)
+		    	branches.add(cleanstr[2]);
+		    else
+			    branches.add(cleanstr[1]);
 		}
 		System.out.println("Number of branches in repository: " + num_of_branches);
 		stdInput.close();
@@ -91,57 +104,205 @@ public class analysis {
 		while ((s = stdInput.readLine()) != null) {
 		 //   System.out.println(s);
 		    		    
-		    Integer i = commiters.get(s);
-		    if (i == null)
-		    	i=1;
+		    Committer c = committers.get(s);
+		    if (c == null){
+		    	c = new Committer();
+		    	c.numOfCommits = 1;
+		    }
 		    else
-		    	i++;
-		    commiters.put(s, i);	    	
+		    	c.numOfCommits = c.numOfCommits + 1;
+		    
+		    committers.put(s, c);	    	
 		}
 		
-		System.out.println("Number of commiters in repository: " + commiters.size());
+		System.out.println("Number of commiters in repository: " + committers.size());
 		
-//		Set<String> keys = commiters.keySet();
-//		for(String str: keys)
-//		{
-//		    System.out.println(str + ", " + commiters.get(str));
-//		}
-//		
+		Set<String> keys = committers.keySet();
+		int i=0;
+		for(String str: keys)
+		{
+			i+=committers.get(str).numOfCommits;
+		    
+		}
+		CommitCount = i;
+		System.out.println("Commit count: " + i);
+		
 		stdInput.close();
 		
 		//4th
-		ArrayList<BranchInfo> branchInfoArray = new ArrayList<BranchInfo>();
+		
+		HashMap<String,String> tag_relations = new HashMap<String,String>();
+		ArrayList<String> tag_list = new ArrayList<String>();
+		
+		//get tag list for each commit
+		Process get_tags=null;
+		ProcessBuilder get_tags_builder = new ProcessBuilder("git","tag");
+		get_tags_builder.directory(new File(directory));
+		get_tags = get_tags_builder.start();
+		BufferedReader tag_input =  new BufferedReader(new InputStreamReader(get_tags.getInputStream()));
+		String temp;
+		while ((temp = tag_input.readLine()) != null) {
+			tag_list.add(temp);
+		}
+		tag_input.close();
+		for(String str: tag_list){
+			get_tags_builder = new ProcessBuilder("git","rev-list","-n","1",str);
+			get_tags_builder.directory(new File(directory));
+			get_tags = get_tags_builder.start();
+			tag_input =  new BufferedReader(new InputStreamReader(get_tags.getInputStream()));
+			while ((temp = tag_input.readLine()) != null) {
+				tag_relations.put(temp,str);
+			}
+			tag_input.close();
+		}
+		
+		branchInfoArray = new ArrayList<BranchInfo>();
+		int k=0;
 		for (String str: branches){
 			
 			BranchInfo branchinfo = new BranchInfo();
-			branchinfo.BranchName = str;
+			branchinfo.branchName = str;
 			CommitInfo commitinfo;
-			pb = new ProcessBuilder("git","--no-pager","log","--pretty=format:'%H%n%aN%n%ad%n%s%n%n%b%nendofbody'","--date-order",str,"--reverse");
+			pb = new ProcessBuilder("git","--no-pager","log","--pretty=format:%H%n%aN%n%ad%n%s%n%n%b%nendofbody","--date-order",str,"--reverse");
 			pb.directory(new File(directory));
 			proc = pb.start();
 			stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));	
 			
 			while ((s = stdInput.readLine()) != null) {
 				
+				k++;
 				commitinfo = new CommitInfo();
 				commitinfo.id = s;
+				commitinfo.tag = tag_relations.get(s);
 				s = stdInput.readLine();
 				commitinfo.author = s;
 				s = stdInput.readLine();
 				commitinfo.date = s;
 				
 				StringBuilder strbuilder = new StringBuilder("");
-				while(!(s=stdInput.readLine()).equals("endofbody")){
+				while(!((s=stdInput.readLine()).equals("endofbody"))){
 					strbuilder.append(s);
 				}
 				commitinfo.message = strbuilder.toString();
 				
-				branchinfo.CommitList.add(commitinfo);
+				branchinfo.commitList.add(commitinfo);
 			}
-			branchinfo.CreationDate = branchinfo.CommitList.get(0).date;
-			branchinfo.LastModified = branchinfo.CommitList.get(branchinfo.CommitList.size()-1).date;
+			branchinfo.creationDate = branchinfo.commitList.get(0).date;
+			branchinfo.lastModified = branchinfo.commitList.get(branchinfo.commitList.size()-1).date;
+			System.out.println(branchinfo.branchName + " Creation Date: " + branchinfo.creationDate + " \nLast Modified: " + branchinfo.lastModified + "\n");
 			branchInfoArray.add(branchinfo);
+			stdInput.close();
 		}
+		
+		System.out.println("Number of commits: " + k);
+		
 
+	}
+	
+	public void BranchStatistics(){
+		
+		Set<String> keys = committers.keySet();
+		for(String str: keys){
+			Committer c = committers.get(str);
+			c.commitPercentage = (double) (( c.numOfCommits / CommitCount ) * 100);
+			committers.put(str, c);
+		}
+		
+		for(BranchInfo b: branchInfoArray){
+			b.commitPercentage = (double) (( b.commitList.size() / CommitCount ) * 100);
+			
+			HashMap<String, Integer> commitsToBranch = new HashMap<String, Integer>();
+			for(CommitInfo c: b.commitList){
+				
+				Integer i = commitsToBranch.get(c.author);
+				if(i == null)
+					i = 1;
+				else
+					i++;
+				commitsToBranch.put(c.author, i);
+			}
+			
+			keys = commitsToBranch.keySet();
+			for(String str: keys){
+				Double percentage = (double) (( commitsToBranch.get(str) / b.commitList.size() ) * 100);
+				b.percentPerCommiter.put(str, percentage);
+			}
+ 		}
+				
+	}
+	
+	public void calculateActiveTime() throws IOException{
+		String directory = "C:\\Users\\Aruil\\Documents\\temp_git_repo\\jgit";
+
+		Process proc = null;
+		ProcessBuilder pb;
+		pb = new ProcessBuilder("git", "--no-pager","log","--pretty=format:%ct","--date-order","--reverse");
+		pb.directory(new File(directory));
+		proc = pb.start();
+		
+		BufferedReader stdInput = new BufferedReader(new 
+		     InputStreamReader(proc.getInputStream()));
+		String s = null;
+		String temp = null;
+		String firstDate = null;
+		String lastDate;
+		boolean flag = false;
+		while ((s = stdInput.readLine()) != null) {
+			if(flag == false){
+				flag = true;
+				firstDate = s;
+			}
+			temp = s;
+		}
+		lastDate = temp;
+		
+		Double days, months, years;
+		Integer diff = Integer.valueOf(lastDate) - Integer.valueOf(firstDate);
+		days = (double) (diff / (3600*24));
+		months = days / 30 ;
+		years = months / 12;
+		
+		Set<String> keys = committers.keySet();
+		for(String str: keys){
+			Committer c = committers.get(str);
+			c.commitsPerDay = c.numOfCommits / days;
+			c.commitsPerMonth = c.numOfCommits / months;
+			c.commitsPerYear = c.numOfCommits / years;
+			
+			committers.put(str, c);
+		}
+	}
+	
+	public void calculateNumOfChanges() throws IOException{
+		String directory = "C:\\Users\\Aruil\\Documents\\temp_git_repo\\jgit";
+
+		Process proc = null;
+		ProcessBuilder pb;
+		pb = new ProcessBuilder("git", "--no-pager","log","--pretty=short","--numstat","--all");
+		pb.directory(new File(directory));
+		proc = pb.start();
+		
+		BufferedReader stdInput = new BufferedReader(new 
+		     InputStreamReader(proc.getInputStream()));
+		
+		String s=null;
+		while ((s = stdInput.readLine()) != null) {
+			
+			String[] parts = s.split(" ");
+			if( parts[0].equals("commit")){
+				
+				for(int i=0; i<4; i++)
+					s=stdInput.readLine();
+				
+				while (!(s = stdInput.readLine()).equals("\n")) {
+					
+					parts = s.split("\t");
+					if(!parts[0].equals("-")){
+						numOfInsertions += Integer.valueOf(parts[0]);
+						numOfDeletions += Integer.valueOf(parts[1]);
+					}
+				}
+			}
+		}
 	}
 }
